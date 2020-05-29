97cc690cbab8092bff0881c74968649474df8e69
praveen10pathak85 <praveen85pathak@gmal.com>
2020-05-29 12:31:37 +0530

    Updates

---
+++ precache-manifest.87a826ac48fcd4534fc6c0a1b9717743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3ef64013281e724db44cb40c2d6a16",
+    "url": "/TESTREACTCURD/index.html"
+  },
+  {
+    "revision": "a85fd88f42d538a47b0c",
+    "url": "/TESTREACTCURD/static/css/main.8a15d8e6.chunk.css"
+  },
+  {
+    "revision": "c970deeb2ba59929d794",
+    "url": "/TESTREACTCURD/static/js/2.efaee7f4.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/TESTREACTCURD/static/js/2.efaee7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85fd88f42d538a47b0c",
+    "url": "/TESTREACTCURD/static/js/main.bab8eda5.chunk.js"
+  },
+  {
+    "revision": "4574f5fc4cc9fc85b23a",
+    "url": "/TESTREACTCURD/static/js/runtime-main.0bb7f0f7.js"
+  }
+]);
